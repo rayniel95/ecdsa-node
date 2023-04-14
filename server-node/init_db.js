8844d107ca43ca8d3bff5830f8e9e60a6bca641c
rayniel95 <rainyel.ramos@gmail.com>
2023-04-14 16:49:54 -0400

    add default balance

--- server-node/init_db.js
+++ server-node/init_db.js
@@ -9,18 +9,27 @@ export async function initDatabase(account) {
     await account.findOrCreate({
         where: {
             address: '0x8b2ff5c6bb4685e48707fc69a3729ee9b53162d7'
+        },
+        defaults:{
+            balance: 100
         }
     })
 
     await account.findOrCreate({
         where: {
             address: '0x456cfd7ef7554290a1931cdd9f1d28623723872e'
+        },
+        defaults:{
+            balance: 100
         }
     })
 
     await account.findOrCreate({
         where: {
             address: '0x70b4d825a9ab0bfebb438d972d9d9a38690cfdd1'
+        },
+        defaults: {
+            balance: 100
         }
     })
 }
