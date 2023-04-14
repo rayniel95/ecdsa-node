0b07677fdedbf664edba8d798569a76fef0aaf52
rayniel95 <rainyel.ramos@gmail.com>
2023-04-14 16:29:30 -0400

    airdrop some address

--- server-node/init_db.js
+++ server-node/init_db.js
@@ -2,25 +2,25 @@ import { Model } from "sequelize"
 
 
 /**
- * 
+ * init the database if there is not item.
  * @param {Model} account 
  */
 export async function initDatabase(account) {
     await account.findOrCreate({
         where: {
-            address: 'here one address'
+            address: '0x8b2ff5c6bb4685e48707fc69a3729ee9b53162d7'
         }
     })
 
     await account.findOrCreate({
         where: {
-            address: 'here two address'
+            address: '0x456cfd7ef7554290a1931cdd9f1d28623723872e'
         }
     })
 
     await account.findOrCreate({
         where: {
-            address: 'here three address'
+            address: '0x70b4d825a9ab0bfebb438d972d9d9a38690cfdd1'
         }
     })
 }
