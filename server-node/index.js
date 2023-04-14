325eb3e3c07ba767a0e100e7ce8f83bcf871159c
rayniel95 <rainyel.ramos@gmail.com>
2023-04-13 22:47:58 -0400

    reestructure project

--- server-node/index.js
+++ server-node/index.js
@@ -3,56 +3,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const defineModels = require('./models')
 
 app.use(cors());
 app.use(express.json());
 
 const sequelize = new Sequelize('sqlite::memory:');
-
-const Transaction = sequelize.define('Transaction', {
-  // Model attributes are defined here
-  timestamp: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  nonce: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  fromAddress: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  toAddress: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  }
-}, {
-  // Other model options go here
-});
-
-const Account = sequelize.define('Account', {
-  // Model attributes are defined here
-  Address: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  balance: {
-    type: DataTypes.BIGINT,
-    allowNull: false,
-    defaultValue: 0
-  }
-}, {
-  // Other model options go here
-});
-
-Account.hasMany(Transaction)
-Transaction.belongsTo(Account)
+const {Account, Transaction} = defineModels(sequelize);
 
 await sequelize.sync()
 
 
+//TODO - close the db connection before shutdown
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
   const balance = balances[address] || 0;
