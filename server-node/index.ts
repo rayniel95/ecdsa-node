5ef745d06061ce1f1eb71edb4f87a5b9fa89eed7
rayniel95 <rainyel.ramos@gmail.com>
2023-04-16 16:08:17 -0400

    comment to compile

--- server-node/index.ts
+++ server-node/index.ts
@@ -24,7 +24,7 @@ db.sequelize.sync()
 //TODO - close the db connection before shutdown
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
-  const balance = balances[address] || 0;
+  const balance = 0
   res.send({ balance });
 });
 
@@ -34,21 +34,21 @@ app.post("/send", (req, res) => {
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
-  }
+  // if (balances[sender] < amount) {
+  //   res.status(400).send({ message: "Not enough funds!" });
+  // } else {
+  //   balances[sender] -= amount;
+  //   balances[recipient] += amount;
+  //   res.send({ balance: balances[sender] });
+  // }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
 
-function setInitialBalance(address) {
-  if (!balances[address]) {
-    balances[address] = 0;
-  }
+function setInitialBalance(address: any) {
+  // if (!balances[address]) {
+  //   balances[address] = 0;
+  // }
 }
