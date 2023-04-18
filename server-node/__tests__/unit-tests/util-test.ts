ceab4d8315fcf52d945571a7c1e3bb8fe57be205
rayniel95 <rainyel.ramos@gmail.com>
2023-04-18 13:20:22 -0400

    add integration test to post method

---
+++ server-node/__tests__/unit-tests/util-test.ts
@@ -0,0 +1,7 @@
+import { verifySignature, getAddressFromSignature } from "../../utils";
+
+describe("test util funtions", () => {
+    test("some fake test", () => {
+        expect(0).toEqual(0);
+    });
+});
