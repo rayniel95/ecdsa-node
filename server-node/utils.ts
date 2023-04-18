d2b7ac1c79da1e824eeca4593982dcb8304ad6cc
rayniel95 <rainyel.ramos@gmail.com>
2023-04-18 12:56:13 -0400

    fix address format

--- server-node/utils.ts
+++ server-node/utils.ts
@@ -1,7 +1,9 @@
 import { keccak256 } from "ethereum-cryptography/keccak";
 import { utf8ToBytes } from "ethereum-cryptography/utils";
-import secp from "ethereum-cryptography/secp256k1";
+import * as secp from "ethereum-cryptography/secp256k1";
 import { toHex } from "ethereum-cryptography/utils";
+import { sha256 } from "ethereum-cryptography/sha256";
+
 
 export function hashMessage(message: string) {
     return keccak256(utf8ToBytes(message));
@@ -24,7 +26,16 @@ function getAddress(publicKey: Uint8Array) {
 }
 
 export function getAddressFromSignature(message: string, signature: string, recoveryBit: number) {
-    return toHex(getAddress(
+    return `0x${toHex(getAddress(
         recoverKey(message, signature, recoveryBit))
+    )}`
+}
+
+export async function getSignature(message: string, privateKey: string) {
+    const [signature, recoverBit] = await secp.sign(
+        hashMessage(message),
+        sha256(utf8ToBytes(privateKey)),
+        { recovered: true }
     )
+    return [toHex(signature), recoverBit]
 }
