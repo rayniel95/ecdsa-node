ce05f320a21fb2d17f618f5b24d244fb1c8cfb60
rayniel95 <rainyel.ramos@gmail.com>
2023-04-21 14:58:09 -0400

    add static typed props to component

--- client/components/Wallet.tsx
+++ client/components/Wallet.tsx
@@ -1,10 +1,21 @@
 import server from "./server";
+import { Dispatch, SetStateAction } from "react";
 
-export function Wallet({ address, setAddress, balance, setBalance }) {
+
+interface WalletProps {
+  address: string; 
+  setAddress: Dispatch<SetStateAction<string>>;
+  balance: number; 
+  setBalance: Dispatch<SetStateAction<number>>
+}
+
+export function Wallet({ address, setAddress, balance, setBalance }: WalletProps) {
+
   async function onChange(evt) {
     const address = evt.target.value;
+
     setAddress(address);
-    if (address) {
+    if (address && address.split('x')[1].length == 40) {
       const {
         data: { balance },
       } = await server.get(`balance/${address}`);
