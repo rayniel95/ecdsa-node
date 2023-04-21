a7dfba73deaefde7a43368ee8056ec3321dae413
rayniel95 <rainyel.ramos@gmail.com>
2023-04-21 16:48:54 -0400

    complete wallet component

--- client/components/Wallet.tsx
+++ client/components/Wallet.tsx
@@ -1,27 +1,30 @@
 import server from "./server";
-import { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 
 
-interface WalletProps {
-  address: string; 
+export interface WalletProps {
+  address: string;
   setAddress: Dispatch<SetStateAction<string>>;
-  balance: number; 
+  balance: number;
   setBalance: Dispatch<SetStateAction<number>>
 }
 
 export function Wallet({ address, setAddress, balance, setBalance }: WalletProps) {
+  const [isValidPublicKey, setIsValidPublicKey] = useState(false);
 
-  async function onChange(evt) {
-    const address = evt.target.value;
+  async function onChange(evt: React.ChangeEvent<HTMLInputElement>) {
+    const address: string = evt.target.value;
 
     setAddress(address);
-    if (address && address.split('x')[1].length == 40) {
+
+    if (address && address.split('x').length > 1 && address.split('x')[1].length === 40 && isHex(address) && address.split('x')[0]==='0') {
       const {
         data: { balance },
       } = await server.get(`balance/${address}`);
+      setIsValidPublicKey(true)
       setBalance(balance);
     } else {
-      setBalance(0);
+      setIsValidPublicKey(false)
     }
   }
 
@@ -34,7 +37,14 @@ export function Wallet({ address, setAddress, balance, setBalance }: WalletProps
         <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
       </label>
 
-      <div className="balance">Balance: {balance}</div>
+      {
+        isValidPublicKey? <div className="balance">Balance: {balance}</div>: <div className="balance">That is not a valid public key</div>
+      }
     </div>
   );
+}
+
+function isHex(h: string) {
+  const a = BigInt(h);
+  return `0x${a.toString(16)}` === h.toLowerCase()
 }
