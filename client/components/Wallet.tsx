204b8e2a4d450d7016f49333733bdde0fe905705
rayniel95 <rainyel.ramos@gmail.com>
2023-04-23 18:22:00 -0400

    create a small wallet component

--- client/components/Wallet.tsx
+++ client/components/Wallet.tsx
@@ -1,6 +1,7 @@
 import server from "./server";
-import React, { Dispatch, SetStateAction, useState } from "react";
-
+import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
+import { useAccount } from "wagmi";
+import { AddressInput } from "./AddressInput";
 
 export interface WalletProps {
   address: string;
@@ -9,42 +10,17 @@ export interface WalletProps {
   setBalance: Dispatch<SetStateAction<number>>
 }
 
-export function Wallet({ address, setAddress, balance, setBalance }: WalletProps) {
-  const [isValidPublicKey, setIsValidPublicKey] = useState(false);
-
-  async function onChange(evt: React.ChangeEvent<HTMLInputElement>) {
-    const address: string = evt.target.value;
-
-    setAddress(address);
-
-    if (address && address.split('x').length > 1 && address.split('x')[1].length === 40 && isHex(address) && address.split('x')[0]==='0') {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setIsValidPublicKey(true)
-      setBalance(balance);
-    } else {
-      setIsValidPublicKey(false)
-    }
-  }
+export function Wallet({balance, setBalance, address, setAddress}: WalletProps) {
 
   return (
     <div className="container wallet">
       <h1>Your Wallet</h1>
-
-      <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
-      </label>
-
-      {
-        isValidPublicKey? <div className="balance">Balance: {balance}</div>: <div className="balance">That is not a valid public key</div>
-      }
+      <AddressInput
+        balance={balance}
+        setBalance={setBalance}
+        address={address}
+        setAddress={setAddress}
+      />
     </div>
   );
-}
-
-function isHex(h: string) {
-  const a = BigInt(h);
-  return `0x${a.toString(16)}` === h.toLowerCase()
 }
