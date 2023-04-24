85e07e0f46ba3adc0b705511421aeee240e1dd05
rayniel95 <rainyel.ramos@gmail.com>
2023-04-24 19:40:43 -0400

    use effect hook to syncronize address in component

--- client/components/AddressInput.tsx
+++ client/components/AddressInput.tsx
@@ -1,17 +1,13 @@
 import server from "./server";
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { WalletProps } from "./Wallet";
 
-//TODO - solve the problem of balance update when metamask is connected
 export function AddressInput({ address, setAddress, balance, setBalance }: WalletProps) {
   const [isValidPublicKey, setIsValidPublicKey] = useState(false);
 
-  async function onChange(evt: React.ChangeEvent<HTMLInputElement>) {
-    const address: string = evt.target.value;
-
-    setAddress(address);
+  useEffect(() => {
     setIsValidPublicKey(validPublickKey(address))
-  }
+  }, [address]);
 
   async function onClick() {
     if (validPublickKey(address)) {
@@ -19,14 +15,14 @@ export function AddressInput({ address, setAddress, balance, setBalance }: Walle
         data: { balance },
       } = await server.get(`balance/${address}`);
       setBalance(balance);
-    } 
+    }
   }
 
   return (
     <div className="container wallet">
       <label>
         Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        <input placeholder="Type an address, for example: 0x1" value={address} onChange={(e)=>setAddress(e.target.value)}></input>
       </label>
       {
         isValidPublicKey ? <div className="balance">Balance: {balance}</div> : <div className="balance">That is not a valid public key</div>
@@ -36,7 +32,8 @@ export function AddressInput({ address, setAddress, balance, setBalance }: Walle
   );
 }
 
-function validPublickKey(publicKey:string) {
+function validPublickKey(publicKey: string) {
+  //TODO - use a regex to input validation
   return publicKey && publicKey.split('x').length > 1 && publicKey.split('x')[1].length === 40 && isHex(publicKey) && publicKey.split('x')[0] === '0'
 }
 
