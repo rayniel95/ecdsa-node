f967f781e9eb214b259cc964efa31ccf1da8e008
rayniel95 <rainyel.ramos@gmail.com>
2023-04-25 16:56:52 -0400

    sign message using metamask

--- client/components/Transfer.tsx
+++ client/components/Transfer.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import server from "./server";
+import { useAccount, useSignMessage } from "wagmi";
+import { getRandomBytesSync } from "ethereum-cryptography/random";
 
 
 export function Transfer({ address, setBalance }) {
@@ -7,17 +9,39 @@ export function Transfer({ address, setBalance }) {
   const [recipient, setRecipient] = useState("");
   const [privateKey, setPrivateKey] = useState("")
 
+  const { isConnected } = useAccount()
+  const { signMessage } = useSignMessage({
+    async onSuccess(data, variables: { message: string }) {
+     await onSuccessSignature(data, variables, setBalance)
+    },
+  })
+
   async function transfer(evt: { preventDefault: () => void; }) {
     evt.preventDefault();
+    const message = {
+      fromAddress: address,
+      toAddress: recipient,
+      amount: sendAmount,
+      timestamp: Date.now(),
+      nonce: new DataView(
+        getRandomBytesSync(4).buffer, 0
+      ).getUint32(0, false)
+    }
+    let signature: string
+    if (isConnected) {
+      signMessage({ message: JSON.stringify(message) })
+      return
+    } else {
+      signature = signMessagePrivKey(message, privateKey)
+    }
 
+    const headers = {
+      signature
+    }
     try {
       const {
         data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
+      } = await server.post(`send`, message, { headers });
       setBalance(balance);
     } catch (ex) {
       alert(ex.response.data.message);
@@ -30,10 +54,10 @@ export function Transfer({ address, setBalance }) {
 
       <label>
         Private Key
-        <input
+        <input disabled={isConnected}
           placeholder="1, 2, 3..."
           value={privateKey}
-          onChange={(e)=>setPrivateKey(e.target.value)}
+          onChange={(e) => setPrivateKey(e.target.value)}
         ></input>
       </label>
       <label>
@@ -41,7 +65,7 @@ export function Transfer({ address, setBalance }) {
         <input
           placeholder="1, 2, 3..."
           value={sendAmount}
-          onChange={(e)=>setSendAmount(e.target.value)}
+          onChange={(e) => setSendAmount(e.target.value)}
         ></input>
       </label>
 
@@ -50,11 +74,29 @@ export function Transfer({ address, setBalance }) {
         <input
           placeholder="Type an address, for example: 0x2"
           value={recipient}
-          onChange={(e)=>setRecipient(e.target.value)}
+          onChange={(e) => setRecipient(e.target.value)}
         ></input>
       </label>
 
       <input type="submit" className="button" value="Transfer" />
     </form>
   );
+}
+
+async function onSuccessSignature(data, variables: { message: string }, setBalance) {
+  const headers = {
+    signature: data
+  }
+  try {
+    const {
+      data: { balance },
+    } = await server.post(`send`, JSON.parse(variables.message), { headers });
+    setBalance(balance);
+  } catch (ex) {
+    alert(ex.response.data.message);
+  }
+}
+
+function signMessagePrivKey(message: any, privateKey: string): string {
+  return ''
 }
