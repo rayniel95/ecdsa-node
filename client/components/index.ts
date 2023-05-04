bf1e882749dc1969d105750973fac5349ec1979b
rayniel95 <rainyel.ramos@gmail.com>
2023-05-04 23:30:37 +0000

    Deploying to gh-pages from @ rayniel95/ecdsa-node@5d96c41c7f3c2caa2a0ad27edd0bf9a13de4d4e7 🚀

--- client/components/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Transfer'
-export * from './Wallet'
--- server-node/__tests__/unit-tests/util-test.ts
+++
@@ -1,7 +0,0 @@
-import { verifySignature, getAddressFromSignature } from "../../utils";
-
-describe("test util funtions", () => {
-    test("some fake test", () => {
-        expect(0).toEqual(0);
-    });
-});
